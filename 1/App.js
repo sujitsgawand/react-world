cd739446cae5e4ab162bfcada91db5c051d16c48
CSTNHHC <gawandsujits@johndeere.com>
2024-04-12 20:48:14 +0530

    Understanding react basics

---
+++ 1/App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Let's go with new World! Happy"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
