58e298bc81f34053309b86616bb2bae8677bd180
CSTNHHC <gawandsujits@johndeere.com>
2024-04-13 12:12:36 +0530

    Added scipts and first jsx code

--- 13th-april/App.js
+++ 13th-april/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Let's go with new World! Happy"
-);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Let's go with new World! Happy"
+// );
+
+const jsxHeading = <h1 id="heading">Let's build the world!</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
